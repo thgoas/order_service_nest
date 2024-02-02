345da800e5d77b080758358d4f844353471989cf
thgoas <thgas95@gmail.com>
2024-02-02 06:24:04 -0300

    test: create profile controller tests

--- src/profiles/profiles.controller.spec.ts
+++ src/profiles/profiles.controller.spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 import { CommonModule } from '../common/common.module';
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { fakeProfiles } from './mock_data/fakeProfiles';
+import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 
 describe('ProfilesController', () => {
   let controller: ProfilesController;
+  let profilesServiceMock: ProfilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,26 +17,139 @@ describe('ProfilesController', () => {
     }).compile();
 
     controller = module.get<ProfilesController>(ProfilesController);
+    profilesServiceMock = module.get<ProfilesService>(ProfilesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should throw HttpException for empty string', () => {
-    const data = {
-      description: '',
-      name: '',
-    };
-    expect(() => controller.create(data)).toThrow(
-      new HttpException('cannot be empty!', HttpStatus.FORBIDDEN),
-    );
+  describe('findAll', () => {
+    it('should retunr an array of profiles', async () => {
+      jest
+        .spyOn(profilesServiceMock, 'findAll')
+        .mockResolvedValueOnce(fakeProfiles);
+      const response = await controller.findAll();
+      expect(profilesServiceMock.findAll).toHaveBeenCalledTimes(1);
+      expect(response).toEqual(fakeProfiles);
+    });
   });
-  it('should not throw HttpException for non-empty string', () => {
-    const data = {
-      description: 'exist',
-      name: 'exist',
-    };
-    expect(() => controller.create(data)).not.toThrow();
+  describe('create', () => {
+    it('should create a profile successfully', async () => {
+      const createProfileDto = {
+        name: 'Test Name',
+        description: 'Test Description',
+      };
+
+      const createdProfile = {
+        id: '1',
+        name: 'Test Name',
+        description: 'Test Description',
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      // Mockando o método create do serviço para retornar um perfil criado
+      jest
+        .spyOn(profilesServiceMock, 'create')
+        .mockResolvedValueOnce(createdProfile);
+
+      const result = await controller.create(createProfileDto);
+
+      expect(profilesServiceMock.create).toHaveBeenCalledTimes(1);
+      expect(profilesServiceMock.create).toHaveBeenCalledWith(createProfileDto);
+
+      expect(result).toEqual(createdProfile);
+    });
+
+    it('should handle HttpException for empty name', async () => {
+      const createProfileDto = {
+        name: '',
+        description: 'Test Description',
+      };
+
+      // Mockando o método create do serviço para lançar uma exceção
+      jest
+        .spyOn(profilesServiceMock, 'create')
+        .mockRejectedValueOnce(
+          new HttpException('cannot be empty!', HttpStatus.FORBIDDEN),
+        );
+
+      // Chama o método create do controller e espera que ele lance uma exceção
+      await expect(controller.create(createProfileDto)).rejects.toThrowError(
+        new HttpException('cannot be empty!', HttpStatus.FORBIDDEN),
+      );
+
+      // Verifica se o método create do serviço foi chamado corretamente
+      expect(profilesServiceMock.create).toHaveBeenCalledWith(createProfileDto);
+    });
+
+    it('should handle HttpException for empty description', async () => {
+      const createProfileDto = {
+        name: 'Test Name',
+        description: '',
+      };
+
+      jest
+        .spyOn(profilesServiceMock, 'create')
+        .mockRejectedValueOnce(
+          new HttpException('cannot be empty!', HttpStatus.FORBIDDEN),
+        );
+
+      await expect(controller.create(createProfileDto)).rejects.toThrow(
+        new HttpException('cannot be empty!', HttpStatus.FORBIDDEN),
+      );
+
+      expect(profilesServiceMock.create).toHaveBeenCalledWith(createProfileDto);
+    });
+    it('should handle HttpException for duplicate name', async () => {
+      const createProfileDto = {
+        name: 'Duplicate Name',
+        description: 'Test Description',
+      };
+
+      jest
+        .spyOn(profilesServiceMock, 'create')
+        .mockRejectedValueOnce(
+          new HttpException(
+            'name Duplicate Name already exists in the database!',
+            HttpStatus.UNAUTHORIZED,
+          ),
+        );
+
+      await expect(controller.create(createProfileDto)).rejects.toThrow(
+        new HttpException(
+          'name Duplicate Name already exists in the database!',
+          HttpStatus.UNAUTHORIZED,
+        ),
+      );
+
+      expect(profilesServiceMock.create).toHaveBeenCalledWith(createProfileDto);
+    });
+  });
+  describe('delete', () => {
+    it('should return a deleted profile on success ', async () => {
+      jest
+        .spyOn(profilesServiceMock, 'remove')
+        .mockResolvedValue(fakeProfiles[0]);
+
+      const result = await controller.remove('1');
+
+      expect(result).toEqual(fakeProfiles[0]);
+      expect(profilesServiceMock.remove).toHaveBeenCalledTimes(1);
+      expect(profilesServiceMock.remove).toHaveBeenCalledWith('1');
+    });
+
+    it('should return NotFoundException case the does not exist ', () => {
+      jest
+        .spyOn(profilesServiceMock, 'remove')
+        .mockRejectedValue(new NotFoundException());
+
+      expect(controller.remove('id not exist')).rejects.toThrow(
+        new NotFoundException(),
+      );
+      expect(profilesServiceMock.remove).toHaveBeenCalledTimes(1);
+      expect(profilesServiceMock.remove).toHaveBeenCalledWith('id not exist');
+    });
   });
 });
