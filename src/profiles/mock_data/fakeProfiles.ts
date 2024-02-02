b3a7377a7c7a00a7161a57827a4523df35fa09c0
thgoas <thgas95@gmail.com>
2024-02-02 06:24:42 -0300

    feat: create fake profiles

---
+++ src/profiles/mock_data/fakeProfiles.ts
@@ -0,0 +1,16 @@
+export const fakeProfiles = [
+  {
+    id: '1',
+    name: 'common',
+    description: 'Comum',
+    created_at: new Date('2024-01-01'),
+    updated_at: new Date('2024-01-01'),
+  },
+  {
+    id: '2',
+    name: 'admin',
+    description: 'Administrador',
+    created_at: new Date('2024-01-01'),
+    updated_at: new Date('2024-01-01'),
+  },
+];
