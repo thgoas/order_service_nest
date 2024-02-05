a34044a8430390d2c50a6918c2163f4152339847
thgoas <thgas95@gmail.com>
2024-02-05 18:06:54 -0300

    feat: user services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { EmailValidator } from '../utils/email-validator';
+import { PasswordHasher } from '../utils/password-hasher';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return this.prisma.user.create({
-      data: createUserDto,
-    });
+  async create(createUserDto: CreateUserDto) {
+    const emailValidator = new EmailValidator();
+    if (createUserDto.name === '' || createUserDto.name === null) {
+      throw new HttpException('name cannot be empty!', HttpStatus.FORBIDDEN);
+    }
+    if (createUserDto.email === '' || createUserDto.email === null) {
+      throw new HttpException('email cannot be empty!', HttpStatus.FORBIDDEN);
+    }
+
+    if (!emailValidator.validate(createUserDto.email)) {
+      throw new HttpException('Invalid Email!', HttpStatus.FORBIDDEN);
+    }
+
+    if (createUserDto.password === '' || createUserDto.password === null) {
+      throw new HttpException(
+        'password cannot be empty!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    if (createUserDto.password.length < 6) {
+      throw new HttpException(
+        'Password must be greater than or equal to 6 characters',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    if (createUserDto.profile_id === '' || createUserDto.profile_id === null) {
+      throw new HttpException(
+        'profile_id cannot be empty!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const passwordHash = await PasswordHasher.hashPassword(
+      createUserDto.password,
+    );
+    try {
+      return await this.prisma.user.create({
+        data: {
+          ...createUserDto,
+          password: passwordHash,
+        },
+      });
+    } catch (error) {
+      // console.error(error);
+      if (
+        error.code === 'P2003' &&
+        error.meta.field_name.includes('users_profile_id_fkey')
+      ) {
+        throw new HttpException(
+          'profile_id does not exist!',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    }
   }
 
   findAll() {
