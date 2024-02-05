001fb8be03e57e5d8c65e3e7e8c59f1c3f104382
thgoas <thgas95@gmail.com>
2024-02-05 18:03:46 -0300

    feat: create email-validator

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,13 @@
+export class EmailValidator {
+  private emailPattern: RegExp;
+
+  constructor() {
+    // Expressão regular simples para validar endereços de e-mail
+    this.emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  }
+
+  validate(email: string): boolean {
+    // Método para validar o endereço de e-mail usando a expressão regular
+    return this.emailPattern.test(email);
+  }
+}
