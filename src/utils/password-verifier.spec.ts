1e4d71f47c93c4df8f3f449393ad8fc04757dadc
thgoas <thgas95@gmail.com>
2024-02-05 18:05:27 -0300

    test: create password verifier

---
+++ src/utils/password-verifier.spec.ts
@@ -0,0 +1,27 @@
+import { PasswordHasher } from './password-hasher';
+import { PasswordVerifier } from './password-verifier';
+
+describe('PasswordVerifier', () => {
+  it('should compare passwords and return true for matching passwords', async () => {
+    const password = '123456';
+    const hashedPassword = await PasswordHasher.hashPassword(password);
+    const isMatch = await PasswordVerifier.comparePassword(
+      password,
+      hashedPassword,
+    );
+
+    expect(isMatch).toBe(true);
+  });
+
+  it('should compare passwords and return false for non-matching passwords', async () => {
+    const password = '123456';
+    const wrongPassword = 'senhaIncorreta';
+    const hashedPassword = await PasswordHasher.hashPassword(password);
+    const isMatch = await PasswordVerifier.comparePassword(
+      wrongPassword,
+      hashedPassword,
+    );
+
+    expect(isMatch).toBe(false);
+  });
+});
