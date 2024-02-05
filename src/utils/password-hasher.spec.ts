5fc909466623c489ca9ac48d7d459c4b089920a3
thgoas <thgas95@gmail.com>
2024-02-05 18:04:21 -0300

    test: create password hash

---
+++ src/utils/password-hasher.spec.ts
@@ -0,0 +1,10 @@
+import { PasswordHasher } from './password-hasher';
+
+describe('Password hasher', () => {
+  it('should return a hash', async () => {
+    const password = '123456';
+    const hashedPassword = await PasswordHasher.hashPassword(password);
+    expect(hashedPassword).toBeDefined();
+    expect(hashedPassword.length).toBeGreaterThan(0);
+  });
+});
