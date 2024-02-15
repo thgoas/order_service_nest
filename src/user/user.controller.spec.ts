228425017afdc736645cfd027c46eab95b352b64
thgoas <thgas95@gmail.com>
2024-02-15 10:10:01 -0300

    test: user controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 import { CommonModule } from '../common/common.module'
+import { PrismaService } from '../common/prisma/prisma.service'
+import { MailingService } from '../email/mailing.service'
 
 describe('UserController', () => {
   let controller: UserController
@@ -10,7 +12,7 @@ describe('UserController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [CommonModule],
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, PrismaService, MailingService],
     }).compile()
 
     controller = module.get<UserController>(UserController)
