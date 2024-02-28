122f1390ea06ef3b16a1d489051e98fc6388c3d5
thgoas <thgas95@gmail.com>
2024-02-28 18:23:00 -0300

    feat: create dto and roles

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,1 +1,31 @@
-export class CreateCompanyDto {}
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator'
+
+export class CreateCompanyDto {
+  @IsNotEmpty()
+  @IsString()
+  cin: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 200)
+  name: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 200)
+  fantasy: string
+
+  @IsOptional()
+  @IsDate()
+  created_at?: Date
+
+  @IsOptional()
+  @IsDate()
+  updated_at?: Date
+}
