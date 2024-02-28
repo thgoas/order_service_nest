c1c898c5a8375bc1d2a98c44864ad8b0cedef355
thgoas <thgas95@gmail.com>
2024-02-28 16:55:38 -0300

    feat: added  updated user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,37 +6,73 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  ValidationPipe,
+  Request,
+  ForbiddenException,
 } from '@nestjs/common'
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/create-user-dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { AuthGuard } from '../auth/auth.guards'
+import { RolesGuard } from '../roles.guards'
+import { Roles } from '../decorators/roles.decorator'
+import { Role } from '../enums/role.enum'
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto)
+  @Roles(Role.Master, Role.Admin)
+  async create(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+    @Request() req: any,
+  ) {
+    console.log(createUserDto)
+    const user = req.userProfile
+    return await this.userService.create(createUserDto, user)
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll()
+  @Roles(Role.Master, Role.Admin)
+  findAll(@Request() req: any) {
+    const user = req.userProfile
+    return this.userService.findAll(user)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id)
+  @Roles(Role.Master, Role.Admin)
+  findOne(@Param('id') id: string, @Request() req: any) {
+    const user = req.userProfile
+    return this.userService.findOne(id, user)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto)
+  @Roles(Role.Common, Role.Admin, Role.Master)
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+    @Request() req: any,
+  ) {
+    const user = req.userProfile
+    if (
+      user.profile.name !== Role.Admin &&
+      user.profile.name !== Role.Master &&
+      user.id !== id
+    ) {
+      throw new ForbiddenException(
+        'You are not authorized to access this feature',
+      )
+    }
+    return this.userService.update(id, updateUserDto, user)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id)
+  @Roles(Role.Admin, Role.Master)
+  remove(@Param('id') id: string, @Request() req: any) {
+    const user = req.userProfile
+    return this.userService.remove(id, user)
   }
 }
