f51eba966268ddf12ce98b585bb3cacfbc9a0d92
thgoas <thgas95@gmail.com>
2024-02-28 16:56:28 -0300

    feat: added profile module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { CommonModule } from 'src/common/common.module'
 import { MailingService } from 'src/email/mailing.service'
+import { ProfilesService } from 'src/profiles/profiles.service'
 
 @Module({
   imports: [CommonModule],
   controllers: [UserController],
-  providers: [UserService, MailingService],
+  providers: [UserService, MailingService, ProfilesService],
   exports: [UserService],
 })
 export class UserModule {}
