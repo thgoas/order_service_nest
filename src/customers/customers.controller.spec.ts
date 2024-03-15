7253d021e75be21a172742146e5f9936495def3f
thgoas <thgas95@gmail.com>
2024-03-15 16:37:16 -0300

    test: add providers and imports

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,20 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CustomersController } from './customers.controller';
-import { CustomersService } from './customers.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CustomersController } from './customers.controller'
+import { CustomersService } from './customers.service'
+import { PrismaService } from '../common/prisma/prisma.service'
+import { UploadsService } from '../uploads/uploads.service'
+import { JwtService } from '@nestjs/jwt'
+import { UserService } from '../user/user.service'
+import { MailingService } from '../email/mailing.service'
+import { CommonModule } from '../common/common.module'
+import { BullModule } from '@nestjs/bull'
 
 describe('CustomersController', () => {
-  let controller: CustomersController;
+  let controller: CustomersController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CommonModule, BullModule.registerQueue({ name: 'email' })],
       controllers: [CustomersController],
-      providers: [CustomersService],
-    }).compile();
+      providers: [
+        CustomersService,
+        PrismaService,
+        UploadsService,
+        JwtService,
+        UserService,
+        MailingService,
+      ],
+    }).compile()
 
-    controller = module.get<CustomersController>(CustomersController);
-  });
+    controller = module.get<CustomersController>(CustomersController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
