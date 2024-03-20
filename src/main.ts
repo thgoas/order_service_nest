62391232716e98dd59db12063a22e98e44834fe3
thgoas <thgas95@gmail.com>
2024-03-20 08:51:34 -0300

    refactor: add cors true

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as express from 'express'
 import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
 
   app.use('/uploads', express.static(join(__dirname, '..', '..', 'uploads')))
   await app.listen(3131)
